59ea14407a93d19d3c45bd24aefa16160428181a
Matthew Stone <matt@souldo.net>
2015-05-28 08:05:21 -0700

    Overhauled
    Seperated the initial installer into a separate thing entirely, moved
    back to install.ps1 in the current package directory and added the
    install ps1 to files (for use on clients).

--- templates/install.ps1
+++
@@ -1,4 +0,0 @@
-Invoke-WebRequest http://<%= @puppetserver %>:8140/packages/current/puppet-enterprise-<%= @pe_build %>-x64.msi | \
-Invoke-Command –ScriptBlock { msiexec $_ /q PUPPET_MASTER_SERVER=<%= @puppetserver %> \
-PUPPET_CA_SERVER=<%= @caserver %> }
-
