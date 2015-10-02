f579c621903fbe43c971314b96ebbf22a4595e5b
Matthew Stone <matt@souldo.net>
2015-10-02 07:59:10 -0700

    Updated PS Host
    Fixing issues with PS host

--- manifests/powershell_host.pp
+++ manifests/powershell_host.pp
@@ -1,19 +1,17 @@
 class pe_winagent::powershell_host(
 	$ps_home = 'C:\windows\system32\windowspowershell\v1.0',
-	$ps_module_home = "${ps_home}\\modules"
 ) {
-	file { 'PuppetAgentModule' :
-	  path   => "${ps_home}\\Modules\\PuppetAgent\\PuppetAgent.psm1", 
-	  ensure => directory,
-	  source => "puppet:///modules/${module_name}/PuppetAgent.psm1",
-	  source_permissions => ignore,
-	}
+  file { 'PuppetAgentModuleDirectory' :
+    ensure => directory,
+    path   => "${ps_home}\\Modules\\PuppetAgent",
+  }
 
-#	exec { 'Profile Update' :
-#      command => "Add-Content ${ps_home}\\profile.ps1 \"Import-Module PuppetAgent\"",
-#      refreshonly => true,
-#      provider => powershell,
-#      subscribe   => File['PuppetAgentModule'],
-#    }
+  file { 'PuppetAgentModule' :
+    path   => "${ps_home}\\Modules\\PuppetAgent\\PuppetAgent.psm1", 
+    ensure => file,
+    source => "puppet:///modules/${module_name}/PuppetAgent.psm1",
+    source_permissions => ignore,
+    require => File['PuppetAgentModuleDirectory'],
+  }
 
 }
