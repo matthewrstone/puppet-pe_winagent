99762048d270c58245aa70d91ebe653414bec304
Matt Stone <matt.stone@gettyimages.com>
2017-01-09 13:35:31 -0800

    update tests

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -21,9 +21,10 @@ describe 'pe_winagent', :type => :class do
       is_expected.to contain_pe_staging__file('puppet-enterprise-3.3.0.msi').with({ 
       :source => 'https://s3.amazonaws.com/pe-builds/released/3.3.0/puppet-enterprise-3.3.0.msi',
       :target => '/opt/puppet/mock/3.3.0/windows/puppet-enterprise-3.3.0.msi',})
+
       is_expected.to contain_file('/opt/puppet/mock/3.3.0/install.ps1')
-        .with_content(/\$master = \"puppetmaster.local\"/)
-        .with_content(/\$ca = \"caserver.local\"/)
+        .with_content(/If !(\$Master) { \$Master = \"puppetmaster.local\" }/)
+        .with_content(/If !(\$CAServer) { \$CAServer = \"caserver.local\" }/)
         .with_content(/\$source = \"https:\/\/puppetmaster.local:8140\/packages\/current\/windows\"/)
         .with_content(/\$package = \"puppet-enterprise-3.3.0.msi\"/)
         .with_content(/\$puppet = \"C:\\Program Files \(x86\)\\Puppet Labs\\Puppet Enterprise\\bin\\puppet.bat\"/)
@@ -48,8 +49,8 @@ describe 'pe_winagent', :type => :class do
       :source => 'https://s3.amazonaws.com/pe-builds/released/3.8.0/puppet-enterprise-3.8.0-x64.msi',
       :target => '/opt/puppet/mock/3.8.0/windows/puppet-enterprise-3.8.0-x64.msi',})
       is_expected.to contain_file('/opt/puppet/mock/3.8.0/install.ps1')
-        .with_content(/\$master = \"puppetmaster.local\"/)
-        .with_content(/\$ca = \"caserver.local\"/)
+        .with_content(/If !(\$Master) { \$Master = \"puppetmaster.local\" }/)
+        .with_content(/If !(\$CAServer) { \$CAServer = \"caserver.local\" }/)
         .with_content(/\$source = \"https:\/\/puppetmaster.local:8140\/packages\/current\/windows\"/)
         .with_content(/\$package = \"puppet-enterprise-3.8.0-x64.msi\"/)
         .with_content(/\$puppet = \"C:\\Program Files\\Puppet Labs\\Puppet\\bin\\puppet.bat\"/)
@@ -76,8 +77,8 @@ describe 'pe_winagent', :type => :class do
       :source => 'https://s3.amazonaws.com/puppet-agents/2016.1/puppet-agent/1.4.1/repos/windows/puppet-agent-1.4.1-x64.msi',
       :target => '/opt/puppet/mock/2016.1.1/windows/puppet-agent-1.4.1-x64.msi',})
       is_expected.to contain_file('/opt/puppet/mock/2016.1.1/install.ps1')
-        .with_content(/\$master = \"puppetmaster.local\"/)
-        .with_content(/\$ca = \"caserver.local\"/)
+        .with_content(/If !(\$Master) { \$Master = \"puppetmaster.local\" }/)
+        .with_content(/If !(\$CAServer) { \$CAServer = \"caserver.local\" }/)
         .with_content(/\$source = \"https:\/\/puppetmaster.local:8140\/packages\/current\/windows\"/)
         .with_content(/\$package = \"puppet-agent-1.4.1-x64.msi\"/)
         .with_content(/\$puppet = \"C:\\Program Files\\Puppet Labs\\Puppet\\bin\\puppet.bat\"/)
