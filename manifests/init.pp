c1f72a90d8f8c9c3158cefd5d9b83c03488670be
Matt Stone <matt.stone@gettyimages.com>
2017-01-19 04:59:27 -0800

    puppet 4 refactoring

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,32 @@
 # Puppet Entprise Windows 'Repo'
 class pe_winagent(
-  $install_dir  = undef,
-  $puppetserver = lookup('pe_winagent::puppetserver', String, 'first', $settings::server),
-  $caserver     = lookup('pe_winagent::caserver', String, 'first', $settings::server)
+  String $server = lookup(
+    'pe_winagent::server', String, 'first', $settings::server
+  ),
+  String $ca_server = lookup(
+    'pe_winagent::ca_server', String, 'first', $settings::server
+  ),
+  String $install_dir = lookup(
+    'pe_winagent::install_dir', String, 'first', undef
+  ),
+  Enum['automatic','manual','disabled'] $startupmode  = lookup(
+    'pe_winagent::startupmode', String, 'first', undef
+  ),
+  String $environment = lookup(
+    'pe_winagent::environment', String, 'first', undef
+  ),
+  String $accountuser = lookup(
+    'pe_winagent::accountuser', String, 'first', undef
+  ),
+  String $accountpass = lookup(
+    'pe_winagent::accountpass', String, 'first', undef
+  ),
+  String $accountdomain = lookup(
+    'pe_winagent::accountdomain', String, 'first', undef
+  ),
+  String $tempfolder = lookup(
+    'pe_winagent::tempfolder', String, 'first', undef
+  )
 ) {
 
   contain pe_repo
@@ -10,26 +34,11 @@ class pe_winagent(
   $s3_link     = 'https://s3.amazonaws.com'
   $win_dir     = "${public_dir}/${::pe_build}/windows"
   $build_dir   = chop(chop($::pe_build))
-
-  if $::aio_agent_version {
-    $puppet_root = 'C:\Program Files\Puppet Labs'
-    $msi         = "puppet-agent-${::aio_agent_version}-x64.msi"
-    $s3_path     = "puppet-agents/${build_dir}/puppet-agent/${::aio_agent_version}/repos/windows"
-    $s3_url      = "${s3_link}/${s3_path}/${msi}"
-    $puppet_bat  = "${puppet_root}\\Puppet\\bin\\puppet.bat"
-  } else {
-    if $::pe_build =~ /^3.3/ {
-      $msi = "puppet-enterprise-${::pe_build}.msi"
-      $puppet_root = 'C:\Program Files (x86)\Puppet Labs'
-      $puppet_bat  = "${puppet_root}\\Puppet Enterprise\\bin\\puppet.bat"
-    } else {
-      $msi = "puppet-enterprise-${::pe_build}-x64.msi"
-      $puppet_root = 'C:\Program Files\Puppet Labs'
-      $puppet_bat  = "${puppet_root}\\Puppet\\bin\\puppet.bat"
-    }
-    $s3_path    = "pe-builds/released/${::pe_build}"
-    $s3_url     = "${s3_link}/pe-builds/released/${::pe_build}/${msi}"
-  }
+  $puppet_root = 'C:\Program Files\Puppet Labs'
+  $msi         = "puppet-agent-${::aio_agent_version}-x64.msi"
+  $s3_path     = "puppet-agents/${build_dir}/puppet-agent/${::aio_agent_version}/repos/windows"
+  $s3_url      = "${s3_link}/${s3_path}/${msi}"
+  $puppet_bat  = "${puppet_root}\\Puppet\\bin\\puppet.bat"
 
   file { $win_dir : ensure => directory }
 
